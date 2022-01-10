c26069cba97c6ebc40605505de9a6a67f0382e30
prasys <pradeesh@prasys.info>
2022-01-11 11:13:36 +1300

    day 22 solution

---
+++ day22/day22.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+        
+    def getHeight(self,root):
+        if root == None:
+            return -1
+        leftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+        
+        return max(leftHeight,rightHeight) + 1
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
